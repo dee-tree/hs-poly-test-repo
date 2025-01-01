0601420df8595aff39f87a5284e930a0738aa5de
Dmitriy Sokolov <codemitry@gmail.com>
2025-01-01 16:28:07 +0300

    task 2

--- src/Part2/Tasks.hs
+++ src/Part2/Tasks.hs
@@ -12,18 +12,36 @@ data Term = IntConstant { intValue :: Int }          -- числовая кон�
 -- Для бинарных операций необходима не только реализация, но и адекватные
 -- ассоциативность и приоритет
 (|+|) :: Term -> Term -> Term
-(|+|) = notImplementedYet
+(|+|) = BinaryTerm Plus
+infixl 5 |+|
+
 (|-|) :: Term -> Term -> Term
-(|-|) = notImplementedYet
+(|-|) = BinaryTerm Minus
+infixl 5 |-|
+
 (|*|) :: Term -> Term -> Term
-(|*|) = notImplementedYet
+(|*|) = BinaryTerm Times
+infixl 6 |*|
 
 -- Заменить переменную `varName` на `replacement`
 -- во всём выражении `expression`
 replaceVar :: String -> Term -> Term -> Term
-replaceVar varName replacement expression = notImplementedYet
+replaceVar varName replacement expression = case expression of
+   IntConstant _ -> expression
+   Variable name -> if name == varName then replacement else expression
+   BinaryTerm op l r -> BinaryTerm op (replaceVar varName replacement l) (replaceVar varName replacement r)
 
 -- Посчитать значение выражения `Term`
 -- если оно состоит только из констант
 evaluate :: Term -> Term
-evaluate = notImplementedYet
+evaluate expr = case expr of 
+   IntConstant _ -> expr
+   Variable _ -> expr
+   BinaryTerm Plus (IntConstant l) (IntConstant r) -> IntConstant $ l + r
+   BinaryTerm Minus (IntConstant l) (IntConstant r) -> IntConstant $ l - r
+   BinaryTerm Times (IntConstant l) (IntConstant r) -> IntConstant $ l * r
+   BinaryTerm op l r -> case (evl, evr) of
+      (IntConstant _, IntConstant _) -> evaluate $ BinaryTerm op evl evr
+      _ -> BinaryTerm op evl evr
+      where evl = evaluate l
+            evr = evaluate r
