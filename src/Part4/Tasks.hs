4c38591b5fb138211f3662389103c81079ca4ff2
Dmitriy Sokolov <codemitry@gmail.com>
2025-01-02 00:36:30 +0300

    task 4

--- src/Part4/Tasks.hs
+++ src/Part4/Tasks.hs
@@ -16,17 +16,44 @@ rlistToList lst =
 
 -- Реализуйте обратное преобразование
 listToRlist :: [a] -> ReverseList a
-listToRlist = notImplementedYet
+listToRlist l = case reverse l of
+    [] -> REmpty
+    (head : tail) -> listToRlist (reverse tail) :< head
 
 -- Реализуйте все представленные ниже классы (см. тесты)
-instance Show (ReverseList a) where
-    showsPrec = notImplementedYet
-    show = notImplementedYet
-instance Eq (ReverseList a) where
-    (==) = notImplementedYet
-    (/=) = notImplementedYet
+instance (Show a) => Show (ReverseList a) where
+    showsPrec p rl = showParen (p > 10) $ showString "[" . showHelper rl . showString "]" where
+        showHelper :: Show a => ReverseList a -> ShowS
+        showHelper rl = case rl of
+            REmpty -> showString ""
+            (REmpty :< tail) -> showsPrec (p + 1) tail
+            (head :< tail) -> showHelper head . showString "," . showsPrec (p + 1) tail
+    -- show rl = shows rl ""
+
+instance (Eq a) => Eq (ReverseList a) where
+    (==) REmpty REmpty = True
+    (==) (ah :< at) (bh :< bt) = (at == bt) && ah == bh
+    (==) _ _ = False
+--     (/=) this other = not $ this == other
+
 instance Semigroup (ReverseList a) where
+    (<>) REmpty b = b
+    (<>) a REmpty = a
+    (<>) a (h :< t) = (a <> h) :< t
+
 instance Monoid (ReverseList a) where
+    mempty = REmpty
+
 instance Functor ReverseList where
+    fmap f REmpty = REmpty
+    fmap f (h :< t) = fmap f h :< f t
+
 instance Applicative ReverseList where
+    pure v = REmpty :< v
+    (fh :< ft) <*> rl = (fh <*> rl) <> fmap ft rl
+    _ <*> _ =  REmpty
+
 instance Monad ReverseList where
+    return v = REmpty :< v
+    REmpty >>= f = REmpty
+    (h :< t) >>= f = (h >>= f) <> f t
