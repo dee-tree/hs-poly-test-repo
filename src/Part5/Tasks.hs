00bed23090ed71c3723a334f09796818ba296af6
Dmitriy Sokolov <codemitry@gmail.com>
2025-01-02 22:25:50 +0300

    task 5

--- src/Part5/Tasks.hs
+++ src/Part5/Tasks.hs
@@ -4,29 +4,42 @@ import Util(notImplementedYet)
 
 -- Реализуйте левую свёртку
 myFoldl :: (b -> a -> b) -> b -> [a] -> b
-myFoldl = notImplementedYet
+myFoldl facc acc [] = acc
+myFoldl facc acc (head : tail) = myFoldl facc (facc acc head) tail
 
 -- Реализуйте правую свёртку
 myFoldr :: (a -> b -> b) -> b -> [a] -> b
-myFoldr = notImplementedYet
+myFoldr facc acc [] = acc
+myFoldr facc acc (head : tail) = facc head $ myFoldr facc acc tail
 
 -- Используя реализации свёрток выше, реализуйте все остальные функции в данном файле
 
 myMap :: (a -> b) -> [a] -> [b]
-myMap f = notImplementedYet
+myMap f = myFoldr (mapper f) [] where
+    mapper :: (a -> b) -> a -> [b] -> [b]
+    mapper f elem tail = f elem : tail
 
 myConcatMap :: (a -> [b]) -> [a] -> [b]
-myConcatMap f = notImplementedYet
+myConcatMap f = myFoldr (concmapper f) [] where
+    concmapper :: (a -> [b]) -> a -> [b] -> [b]
+    concmapper f elem tail = f elem ++ tail
 
 myConcat :: [[a]] -> [a]
-myConcat = notImplementedYet
+myConcat = myFoldr concatter [] where
+    concatter :: [a] -> [a] -> [a]
+    concatter subl tail = subl ++ tail
 
 myReverse :: [a] -> [a]
-myReverse = notImplementedYet
+myReverse = myFoldr reverser [] where
+    reverser :: a -> [a] -> [a]
+    reverser elem tail = tail ++ [elem]
 
 myFilter :: (a -> Bool) -> [a] -> [a]
-myFilter p = notImplementedYet
+myFilter p = myFoldr (filtrator p) [] where
+    filtrator :: (a -> Bool) -> a -> [a] -> [a]
+    filtrator p elem tail = if p elem then elem : tail else tail
 
 myPartition :: (a -> Bool) -> [a] -> ([a], [a])
-myPartition p = notImplementedYet
-
+myPartition p = myFoldr (divider p) ([], []) where
+    divider :: (a -> Bool) -> a -> ([a], [a]) -> ([a], [a])
+    divider p elem (ok, nok) = if p elem then (elem : ok, nok) else (ok, elem : nok)
